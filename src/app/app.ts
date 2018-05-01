50e4fecc6cb980c5c57d9c05f338ccd6e775815e
Xu, Changhui <changhuixu@hotmail.com>
2018-05-01 16:59:00 -0500

    variable declaration 01

---
+++ src/app/app.ts
@@ -0,0 +1,22 @@
+import { Matrix } from './matrix';
+
+export class App {
+  run(): void {
+    var result = this.calculate(false, 0);
+    console.log(`Result {false, 0}: ${result}`);
+
+    result = this.calculate(true, 0);
+    console.log(`Result {true, 0}: ${result}`);
+
+    // var m = new Matrix([[10,20],[10,20,30]]);
+    // console.log(`matrix sum of ${m.toString()} = ${m.sum()}`);
+  }
+
+  private calculate(condition: boolean, x: number): number {
+    if (condition) {
+      var x = 100;
+      return x;
+    }
+    return x;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+import { App } from "./app/app";
+
 const separator = '='.repeat(80);
 console.log(`\x1b[32m${separator}\x1b[0m\n`);
 
-console.log('Hello World');
+new App().run();
 
 console.log(`\x1b[32m${separator}\x1b[0m`);
