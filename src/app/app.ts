ab472040f8f3ba292da76c3eb262d772292bdfce
Xu, Changhui <changhuixu@hotmail.com>
2018-05-02 10:10:07 -0500

    object assign

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,7 @@ import { Demographic } from './models/demographic.enum';
 import { DrivingAuthorizationStatus } from './models/driving-authorization-status.enum';
 import { DrivingAssignmentsService } from './services/driving-assignments.service';
 import { BadgeStatistics } from './models/badge-statistics';
+import { MFK } from './models/mfk';
 
 export class App {
   readonly assignments: DrivingAssignment[];
@@ -12,6 +13,28 @@ export class App {
   }
 
   run(): void {
-    console.log(this.assignments);
+    const mfk1 = new MFK('050', '03', '0100');
+    //let mfk2 = mfk1;
+    let mfk2 = new MFK('', '', '');
+    Object.assign(mfk2, mfk1);
+
+    console.log(`${JSON.stringify(mfk1)}\t${JSON.stringify(mfk2)}`);
+    mfk1.fund = '010';
+    console.log(`${JSON.stringify(mfk1)}\t${JSON.stringify(mfk2)}`);
+    mfk2.fund = '150';
+    console.log(`${JSON.stringify(mfk1)}\t${JSON.stringify(mfk2)}`);
+
+    const a = [1, 2, 3];
+    //let b = a;    // shallow
+    let b: number[] = [];
+    // Object.assign(b, a); // deep
+    //b = [...a];
+    b = Object.assign([], a); // deep
+
+    console.log(`${a}\t${b}`);
+    a[0] = 0;
+    console.log(`${a}\t${b}`);
+    b[0] = 6;
+    console.log(`${a}\t${b}`);
   }
 }
---
+++ src/app/models/mfk.ts
@@ -0,0 +1,3 @@
+export class MFK {
+  constructor(public fund: string, public org: string, public dept: string) {}
+}
